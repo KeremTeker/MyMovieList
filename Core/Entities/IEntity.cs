371a703774c64e55333faee4f6e0777ac8850f92
Kerem Kenan TEKER <tekerkerem@gmail.com>
2023-01-12 20:15:07 +0300

    12Ocak

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //IEntity olarak implent edilen her class bir veritabanı tablosudur.
     public interface IEntity
